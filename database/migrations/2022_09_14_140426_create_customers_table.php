105bf778404065ec495ccde4d64070d0a441279f
Agus Suartawan <kadeksuwar@gmail.com>
2022-09-14 22:10:39 +0800

    first commit

---
+++ database/migrations/2022_09_14_140426_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik');
+            $table->string('name');
+            $table->text('address');
+            $table->string('phone');
+            $table->enum('gender', ['L', 'P']);
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
