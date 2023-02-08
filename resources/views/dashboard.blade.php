6544a4ebfc2b0ecf284db2986ea427b6a68a270a
agussuartawan <80089992+agussuartawan@users.noreply.github.com>
2023-02-08 09:25:09 +0800

    revisi dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,110 +2,110 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-    </div>
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+</div>
 
-    <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Jumlah Tabungan</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ idr($deposits) }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
+<div class="row">
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            Jumlah Tabungan</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ idr($deposits) }}</div>
                     </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Jumlah Pinjaman</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ idr($loans) }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-danger shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Jumlah Tunggakan</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ idr($arrears) }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                Jumlah Klien</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $clients }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-users fa-2x text-gray-300"></i>
-                        </div>
+                    <div class="col-auto">
+                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <h4>Top 5 klien dengan tabungan terbanyak</h4>
-    <div class="row">
-        <div class="col">
-            <div class="card card-shadow">
-                <div class="card-body p-0">
-                    <table class="table table-bordered table-striped mb-0">
-                        <thead class="text-center">
-                            <tr>
-                                <th>No.</th>
-                                <th>Klien</th>
-                                <th>Saldo Tabungan</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($top_clients as $key => $item)
-                                <tr>
-                                    <td class="text-center">{{ $key + 1 }}</td>
-                                    <td>{{ $item->client->name }}</td>
-                                    <td class="text-right">{{ idr($item->amount) }}</td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="3" class="text-center">Tidak ada data pada tabel</td>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            Jumlah Pinjaman</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ idr($loans) }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-danger shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                            Jumlah Tunggakan</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ idr($arrears) }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                            Jumlah Klien</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $clients }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- <h4>Top 5 klien dengan tabungan terbanyak</h4>
+<div class="row">
+    <div class="col">
+        <div class="card card-shadow">
+            <div class="card-body p-0">
+                <table class="table table-bordered table-striped mb-0">
+                    <thead class="text-center">
+                        <tr>
+                            <th>No.</th>
+                            <th>Klien</th>
+                            <th>Saldo Tabungan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($top_clients as $key => $item)
+                        <tr>
+                            <td class="text-center">{{ $key + 1 }}</td>
+                            <td>{{ $item->client->name }}</td>
+                            <td class="text-right">{{ idr($item->amount) }}</td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="3" class="text-center">Tidak ada data pada tabel</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div> --}}
 @endsection
